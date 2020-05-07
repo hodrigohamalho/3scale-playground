5909505a643c05f5dda65a3e668e69ed6195661a
Rodrigo Ramalho <hodrigohamalho@gmail.com>
2020-05-07 13:19:54 -0300

    adding storage to redis

--- install/install.sh
+++ install/install.sh
@@ -4,19 +4,24 @@
 # Generate a new token here: https://access.redhat.com/terms-based-registry
 oc create secret docker-registry threescale-registry-auth \
    --docker-server=registry.redhat.io \
-   --docker-username="1234|user" \
-   --docker-password="asdadsdasdasdsadasd"
-
-# Instal Redis
-oc new-app registry.redhat.io/rhel8/redis-5 -e REDIS_PASSWORD=redhat --source-secret=threescale-registry-auth
-
-# Install Postgres
-oc new-app registry.redhat.io/rhel8/postgresql-10 --source-secret=threescale-registry-auth -e POSTGRESQL_USER=redhat -e POSTGRESQL_PASSWORD=redhat -e POSTGRESQL_DATABASE=system
+   --docker-username="aaaa" \
+   --docker-password="bbbbbbb"
 
 oc delete secrets backend-redis system-database 
 # Create the secrets for the external databases
 oc create -f 01-secrets.yml 
-# Create a RWX Volume
-oc create -f 02-storage.yml 
+# Create a RWX Volume for 3Scale developer portal and Redis/Postgres PV and PVC
+oc create -f 02-3scale-storage.yml 
+oc create -f 02-redis-storage.yml 
+oc create -f 02-postgres-storage.yml 
+
+# Instal Redis
+oc new-app registry.redhat.io/rhel8/redis-5 --name=redis -e REDIS_PASSWORD=redhat --source-secret=threescale-registry-auth
+oc set volume dc redis --add --overwrite --name=redis-volume-1 --type=persistentVolumeClaim --claim-name=redis-claim --mount-path=/var/lib/redis/data
+
+# Install Postgres
+oc new-app registry.redhat.io/rhel8/postgresql-10 --name=postgres --source-secret=threescale-registry-auth -e POSTGRESQL_USER=redhat -e POSTGRESQL_PASSWORD=redhat -e POSTGRESQL_DATABASE=system
+# TODO POSTGRES STORAGE
+
 # Install the 3Scale API Manager
 oc create -f 03-apimanager.yml
